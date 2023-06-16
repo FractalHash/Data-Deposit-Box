9df39528dcfe1c83f59f1c8cfd9b362df751be53
Kojo Owiredu <owireduosei@outlook.com>
2023-06-16 14:31:58 -0400

    style the landing page

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -4,12 +4,12 @@ import './LandingPage.scss';
 
 const LandingPage = () => {
   return (
-    <div>
-      <div className='logo__container'><img src={logo} alt='' /></div>
-      <h1>Data Deposit Box</h1>
-      <p>Safeguard Your Digital Universe with a single Tap</p>
-      <button>REGISTER</button>
-      <p>Aew you an existing user? <Link to={`/login`}>Login</Link></p>
+    <div className='landing-page__container'>
+      <div className='logo__container'><img className='logo__container-logo' src={logo} alt='' /></div>
+      <h1 className='landing-page__heading'>Data Deposit Box</h1>
+      <p className='landing-page__text'>Safeguard Your Digital Universe with a single Tap</p>
+      <button className='btn'>REGISTER</button>
+      <p className='landing-page__subtext'>Aew you an existing user? <Link className='link' to={`/login`}>Login</Link></p>
     </div>
   );
 };
